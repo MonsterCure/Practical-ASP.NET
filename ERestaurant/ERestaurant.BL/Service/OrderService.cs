cc436cd0e4b070d27f543b04e77f0624b1b74aaf
sedc sedc <sedcsedc08@hotmail.com>
2018-09-10 20:58:59 +0200

    10.09 changes

--- ERestaurant/ERestaurant.BL/Service/OrderService.cs
+++ ERestaurant/ERestaurant.BL/Service/OrderService.cs
@@ -35,7 +35,7 @@ namespace ERestaurant.BL.Service
                         ItemQuantity = o.ItemQuantity
                     }));
 
-                    transaction.Commit();
+                    transaction.Commit(); // SaveChanges() not necessary after the last change to the database and/or before transaction.Commit()
 
                     return new ServiceResult<DtoOrder>
                     {
@@ -56,21 +56,38 @@ namespace ERestaurant.BL.Service
             }
         }
 
-        public ServiceResult<DtoOrder> Edit(DtoOrder item)
+        public ServiceResult<DtoOrder> Edit(DtoOrder item) //only for StatusId, Comments and Table
         {
             using (var transaction = Context.Database.BeginTransaction())
             {
                 try
                 {
-                    Repository.Update(new Order()
+                    if (item == null) //maybe also if(!modelState.IsValid)
                     {
-                        OrderId = item.OrderId,
-                        WhenCreated = item.WhenCreated,
-                        Table = item.Table,
-                        Comments = item.Comments,
-                        StatusId = (byte)item.StatusId,
-                        //ListOrderItems = item.ListOrderItems
-                    });
+                        return new ServiceResult<DtoOrder>
+                        {
+                            Success = false,
+                            Exception = new ArgumentNullException("Order not valid.")
+                        };
+                    }
+
+                    var dbOrder = Repository.DbContext.Orders.FirstOrDefault(o => o.OrderId == item.OrderId);
+
+                    if (dbOrder == null)
+                    {
+                        return new ServiceResult<DtoOrder>
+                        {
+                            Success = false,
+                            ErrorMessage = "Order not found."
+                        };
+                    }
+
+                    //dbOrder.StatusId = (byte)item.StatusId; //to be changed in separte method, becuase of privilege to change it
+                    dbOrder.Table = item.Table;
+                    dbOrder.Comments = item.Comments;
+
+                    Repository.Update(dbOrder); //?
+                    Context.SaveChanges(); //?
 
                     transaction.Commit();
 
@@ -90,7 +107,7 @@ namespace ERestaurant.BL.Service
                         ErrorMessage = ex.Message
                     };
                 }
-            }   
+            }
         }
 
         public ServiceResult<DtoOrder> Load(DtoOrder item)
@@ -171,6 +188,129 @@ namespace ERestaurant.BL.Service
                     Exception = ex,
                     ErrorMessage = ex.Message
                 };
+            }
+        }
+
+        public ServiceResult<DtoOrderItem> SetOrderItem(DtoOrderItem orderItem)
+        {
+            using (var transaction = Context.Database.BeginTransaction())
+            {
+                try
+                {
+                    if (orderItem == null) //maybe also if(!modelState.IsValid)
+                    {
+                        return new ServiceResult<DtoOrderItem>
+                        {
+                            Success = false,
+                            Exception = new ArgumentNullException("Order item not valid.")
+                        };
+                    }
+
+                    var dbOrderItem = Context.OrderItems.FirstOrDefault(oi => oi.OrderItemId == orderItem.OrderItemId);
+
+                    if (dbOrderItem != null)
+                    {
+                        if(orderItem.ItemQuantity > 0)
+                        {
+                            if(dbOrderItem.ItemQuantity == orderItem.ItemQuantity)
+                            {
+                                return new ServiceResult<DtoOrderItem>
+                                {
+                                    Success = true,
+                                    Item = new DtoOrderItem(dbOrderItem)
+                                };
+                            }
+
+                            //exists - update it
+                            dbOrderItem.ItemQuantity = orderItem.ItemQuantity;
+                            Context.SaveChanges();
+
+                            return new ServiceResult<DtoOrderItem>
+                            {
+                                Success = true,
+                                Item = new DtoOrderItem(dbOrderItem)
+                            };
+                        }
+                        else if(orderItem.ItemQuantity == 0)
+                        {
+                            //exists - delete it
+                            Context.OrderItems.Remove(dbOrderItem);
+                            Context.SaveChanges();
+
+                            return new ServiceResult<DtoOrderItem>
+                            {
+                                Success = true
+                            };
+                        }
+                        else
+                        {
+                            //exists - throw error because quantity cannot be negative
+                            return new ServiceResult<DtoOrderItem>
+                            {
+                                Success = false,
+                                ErrorMessage = "Item quantity cannot be negative."
+                            };
+                        }
+                    }
+                    else
+                    {
+                        if (orderItem.ItemQuantity < 1)
+                        {
+                            return new ServiceResult<DtoOrderItem>
+                            {
+                                Success = false,
+                                ErrorMessage = "Item quantity cannot be negative."
+                            };
+                        }
+
+                        var orderExists = Context.Orders.Any(o => o.OrderId == orderItem.OrderId);
+                        var itemExists = Context.Items.Any(i => i.ItemId == orderItem.ItemId);
+
+                        if (!orderExists)
+                            return new ServiceResult<DtoOrderItem>
+                            {
+                                Success = false,
+                                ErrorMessage = "Order not found."
+                            };
+
+                        if(!itemExists)
+                            return new ServiceResult<DtoOrderItem>
+                            {
+                                Success = false,
+                                ErrorMessage = "Item not found."
+                            };
+
+                        //create the order item
+                        var newOrderItem = new OrderItem
+                        {
+                            //OrderItemId = orderItem.OrderItemId,
+                            ItemId = orderItem.ItemId,
+                            OrderId = orderItem.OrderId,
+                            ItemQuantity = orderItem.ItemQuantity
+                        };
+
+                        Context.OrderItems.Add(newOrderItem);
+                        Context.SaveChanges();
+
+                        transaction.Commit();
+
+                        return new ServiceResult<DtoOrderItem>()
+                        {
+                            Success = true,
+                            Item = new DtoOrderItem(newOrderItem)
+                        };
+                    }
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    return new ServiceResult<DtoOrderItem>()
+                    {
+                        Success = false,
+                        Exception = ex,
+                        ErrorMessage = "Order item not set."
+                    };
+                }
             }
         }
     }
