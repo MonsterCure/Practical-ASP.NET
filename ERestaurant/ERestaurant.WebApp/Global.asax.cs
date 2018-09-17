a32a3b67a948c3105658dab3d5589bf0e4e65d62
MonsterCure <mm13586@gmail.com>
2018-09-17 15:26:45 +0200

    Update Global.asax.cs

--- ERestaurant/ERestaurant.WebApp/Global.asax.cs
+++ ERestaurant/ERestaurant.WebApp/Global.asax.cs
@@ -13,6 +13,7 @@ namespace ERestaurant.WebApp
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
