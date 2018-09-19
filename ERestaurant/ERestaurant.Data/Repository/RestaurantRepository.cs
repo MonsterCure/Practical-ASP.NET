f793e4286f37751c3af13c9f6f1371eb7820f1b6
MonsterCure <mm13586@gmail.com>
2018-09-19 09:14:09 +0200

    19.09 changes

---
+++ ERestaurant/ERestaurant.Data/Repository/RestaurantRepository.cs
@@ -0,0 +1,37 @@
+﻿using ERestaurant.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERestaurant.Data.Repository
+{
+    public class RestaurantRepository : BaseRepository, IRepository<Restaurant>
+    {
+        public Restaurant Create(Restaurant item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Restaurant item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Restaurant> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Restaurant GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Restaurant item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
